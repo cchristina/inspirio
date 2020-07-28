30592173a99e3932d1f423c03fe6e8767b899901
christina <christina.cuneo@gmail.com>
2020-07-28 14:42:20 -0700

    readme

--- insp.py
+++ insp.py
@@ -405,7 +405,7 @@ def add_header(response):
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    # app.debug = True
     # make sure templates, etc. are not cached in debug mode
     app.jinja_env.auto_reload = app.debug
 
